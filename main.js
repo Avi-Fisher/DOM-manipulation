a0a4b83e358586c6027b2225269b9641e1db5dfe
Avi Fisher <avifisher82@gmail.com>
2026-02-01 17:53:25 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let inputNumber = document.getElementById("input-number")
+let buttun = document.getElementById("applyBth")
+
+
+buttun.addEventListener('mousedown', () => {
+    if(inputNumber.value === ""){
+        alert("Error, Enter number please")
+    }else{
+        document.getElementById("boxes").element.remove()
+        
+        
+    }
+})
+
+

7dbc33d5e39c2bcbe6a4548e3cea399e354c8069
Avi Fisher <avifisher82@gmail.com>
2026-02-02 11:11:19 +0200

    finsh

--- main.js
+++ main.js
@@ -3,12 +3,29 @@ let buttun = document.getElementById("applyBth")
 
 
 buttun.addEventListener('mousedown', () => {
-    if(inputNumber.value === ""){
+    if (inputNumber.value === "") {
         alert("Error, Enter number please")
-    }else{
-        document.getElementById("boxes").element.remove()
+    } else {
+        let boxes = document.getElementById("boxes")
+        boxes.remove()
+
+        const newDiv = document.createElement('div')
+        newDiv.id = "boxes"
         
-        
+        let body = document.getElementById("body")
+        body.appendChild(newDiv)
+        boxes = document.getElementById("boxes")
+
+        for (let i = 0; i < inputNumber.value; i++) {
+
+
+            let node = document.createElement('div')
+            let textnode = document.createTextNode(i)
+            node.appendChild(textnode)
+
+            boxes.appendChild(node)
+        }
+
     }
 })
 
